9fa42beeb6ef6f098bba4f0affb551c612ecc3eb
Seonmi Park <parkhg09@gmail.com>
2024-10-23 05:53:05 +0900

    운동프로필 상세 추가

--- src/main/java/com/erunjrun/mate/controller/MateController.java
+++ src/main/java/com/erunjrun/mate/controller/MateController.java
@@ -1,10 +1,13 @@
 package com.erunjrun.mate.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.erunjrun.mate.service.MateService;
 
@@ -15,7 +18,13 @@ public class MateController {
 	Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@RequestMapping(value="/mate")
-	public String mateApplication() {
-		return "";
+	public String mate() {
+		return "mate/profileDetail";
+	}
+
+	@RequestMapping(value="/mateAppliaction")
+	public String mateApplication(String fromUserId, String toUserId) {
+		boolean result = mateService.mateApplication(fromUserId, toUserId);
+		return "mate/profileDetail";
 	}
 }
--- src/main/java/com/erunjrun/mate/service/MateService.java
+++ src/main/java/com/erunjrun/mate/service/MateService.java
@@ -1,8 +1,26 @@
 package com.erunjrun.mate.service;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.erunjrun.mate.dao.MateDAO;
 
 @Service
 public class MateService {
+	@Autowired MateDAO mateDAO;
+
+	@Transactional
+	public boolean mateApplication(String fromUserId, String toUserId) {
+		boolean result = false;
+		int appRst = mateDAO.mateApplication(fromUserId, toUserId);
+		int historyRst = mateDAO.mateHistory(fromUserId, toUserId);
+		if(appRst > 0 && historyRst > 0) {
+			result = true;
+		}
+		return result;
+	}
 
 }
