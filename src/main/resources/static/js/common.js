c8cf587508e669e3550eaab48e47ddd3ba4b4c72
park seonmi <parkhg09@gmail.com>
2024-10-23 17:05:00 +0900

    운동프로필 상세

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -33,6 +33,15 @@ $('.toggle-slider').on('click',function(){
 	}
 });
 
+// loading 시 
+function loading(){
+	$('body').append('<div class="loading-bg"><div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>');
+}
+// loading 완료시
+function loadingComplete(){
+	$('.loading-bg').remove();
+}
+
 
 // 태그 체크박스 클릭시 label에 클래스 추가
 
